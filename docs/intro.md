f78b496e4bc0d4918fd9e23d4b4913c085a03f76
danielwang <danielwang130@gmail.com>
2022-02-17 10:11:07 +1100

    chore: remove the md generation code

---
+++ docs/intro.md
@@ -0,0 +1,7 @@
+---
+id: intro
+title: What is Design Token
+custom_edit_url: null
+---
+
+> It's a technology-agnostic architecture and process for scaling design across multiple platforms and devices, including native, and more
--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
     "Brand Guideline" : ['logo', 'colours', 'type', 'icons'],
-    "Design Tokens": ['color', 'fonts', 'font-size', 'line-height', 'radius', 'shadows', 'space', 'opacity',  'breakpoints','z-index'],
+    "Design Tokens": ['intro','color', 'fonts', 'font-size', 'line-height', 'radius', 'shadows', 'space', 'opacity',  'breakpoints','z-index'],
   },
 };
